97f572db84a5814eacf86d1224c9676c3ba0e81d
Ilya Metsler <imetsler@gmail.com>
2015-11-05 15:27:39 +0600

    Module js template added

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+var addWatermark = (function (){
+
+    var initInside = function (){
+        _setUpListners();
+    };
+
+    var _setUpListners = function (){
+        //прослушка событий...
+    };
+
+    return{
+        init: initInside
+    };
+})();
+
+addWatermark.init();
