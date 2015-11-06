b2671fa91a088e6fa92feee28ff72df0823dcf13
ImM0rta1 <webart@bk.ru>
2015-11-06 17:08:38 +0300

    opacity slider ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,30 @@
-$(function() {
-    $('.opacity__slider').slider({
-        range: 'min',
-        value: 50,
-        min: 1,
-        max: 100,
-        slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.value );
-        }
-    });
+var opacitySlider = (function() {
 
-    $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
-});
+    var sliderInit = function() {
+        _setupListners();
+    };
+
+    var _setupListners = function() {
+        _opacity();
+    };
+
+    var _opacity = function() {
+        $('.opacity__slider').slider({
+            range: 'min',
+            min: 0, 
+            max: 1, 
+            step: 0.01, 
+            value: 1,
+            slide: function( event, ui ) {
+                 $('.image-view__watermark').css('opacity', ui.value)
+            }
+        });
+    };
+
+    return {
+        init: sliderInit
+    };
+
+}());
+
+opacitySlider.init();
