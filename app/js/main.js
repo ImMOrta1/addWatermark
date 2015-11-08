3a8dc94e25c80d5c332fc9897670aac7ac072c46
Maks Roschin <dyrnoj@bk.ru>
2015-11-09 00:20:31 +0300

    add change in the grid

--- app/js/main.js
+++ app/js/main.js
@@ -16,13 +16,13 @@ var fileUpload = (function() {
         var $input = $(this),
             val = $input.val().slice(12),
             fileName = $('#fake');
-        fileName.text(val);
+            fileName.text(val);
     };
     var _uploadInfoWat = function (){
         var $input = $(this),
             val = $input.val().slice(12),
             fileName = $('#fakeWat');
-        fileName.text(val);
+            fileName.text(val);
     };
 
 
@@ -51,6 +51,7 @@ var positionModule = (function() {
         _getCoordinates();
         _setCoordinate();
         _setCoordinate(0,0);
+        $('.position-left__list li').on('click touchstart', _gridChange);
     };
 
     var watermark = $('.image-view__water-img'),
@@ -62,50 +63,51 @@ var positionModule = (function() {
         bgContainer = $('.image-view__main-img'),
         positionX = 0,
         positionY = 0;
+        coordinate = 0;
 
     var  _dragWatermark = function() {
         watermark.draggable({
             containment: bgContainer,
             snapTolerance: 0,
             cursor: 'move',
-            drag: function(ev, ui){
-                _getCoordinates();
-            }
+             drag: function(ev, ui){
+                    _getCoordinates();
+                }
         });
     };
 
     var _coordinates = function() {
 
-            _spinnerX.spinner({ min: 0, max: 650 });
-            _spinnerX.on('spin', function(event, ui) {
-                var currentValX = ui.value;
+        _spinnerX.spinner({ min: 0, max: 650 });
+        _spinnerX.on('spin', function(event, ui) {
+            var currentValX = ui.value;
 
-                watermark.css({
-                    left: currentValX + 'px'
-                })
-            });
+            watermark.css({
+                left: currentValX + 'px'
+            })
+        });
 
-            _spinnerY.spinner({ min: 0, max: 530 });
-            _spinnerY.on('spin', function(event, ui) {
-                var currentValY = ui.value;
+        _spinnerY.spinner({ min: 0, max: 530 });
+        _spinnerY.on('spin', function(event, ui) {
+            var currentValY = ui.value;
 
-                watermark.css({
-                    top: currentValY + 'px'
-                })
-            });
+            watermark.css({
+                top: currentValY + 'px'
+            })
+        });
 
-        },
+    },
 
         _getCoordinates = function(elem) {
+            
 
+        if (typeof elem === 'undefined') {
+            elem = watermark;
+        }   
 
-            if (typeof elem === 'undefined') {
-                elem = watermark;
-            }
-
-            var coordinate = elem.position(),
-                positionX  = Math.round( (coordinate.left) ),
-                positionY  = Math.round( (coordinate.top) );
+            coordinate = elem.position();
+            positionX  = Math.round( (coordinate.left) );
+            positionY  = Math.round( (coordinate.top) );
             _setCoordinate(positionX,positionY);
 
             return {
@@ -117,6 +119,59 @@ var positionModule = (function() {
         _setCoordinate = function (x,y) {
             inputX.val(Math.round(x));
             inputY.val(Math.round(y));
+        },
+
+        _gridChange = function () {
+            var 
+                minPosX = 0,
+                minPosY = 0,
+                midPosX = (bgContainer.width() - watermark.width()) / 2,
+                midPosY = (bgContainer.height() - watermark.height()) / 2,
+                maxPosX = bgContainer.width() - watermark.width(),
+                maxPosY = bgContainer.height() - watermark.height(),
+                $this = $(this),
+                position = $this.data('pos');
+                $this.addClass('position-left__item_active').siblings().removeClass('position-left__item_active');
+                switch (position) {
+                    case 'top-left':
+                        watermark.css({'left':minPosX, 'top':minPosY});
+                        _setCoordinate(minPosX,minPosY);
+                        break;
+                    case 'top-center':
+                        watermark.css({'left':midPosX, 'top':minPosY});
+                        _setCoordinate(midPosX,minPosY);
+                        break;
+                    case 'top-right':
+                        watermark.css({'left':maxPosX, 'top':minPosY});
+                        _setCoordinate(maxPosX,minPosY);   
+                        break;
+                    case 'mid-left':
+                        watermark.css({'left':minPosX, 'top':midPosY});
+                        _setCoordinate(minPosX,midPosY);   
+                        break;
+                    case 'mid-center':
+                        watermark.css({'left':midPosX, 'top':midPosY});
+                        _setCoordinate(midPosX,midPosY);   
+                        break;
+                    case 'mid-right':
+                        watermark.css({'left':maxPosX, 'top':midPosY});
+                        _setCoordinate(maxPosX,midPosY);   
+                        break;
+                    case 'btm-left':
+                        watermark.css({'left':minPosX, 'top':maxPosY});
+                        _setCoordinate(minPosX,maxPosY);
+                        break;
+                    case 'btm-center':
+                        watermark.css({'left':midPosX, 'top':maxPosY});
+                        _setCoordinate(midPosX,maxPosY);
+                        break;
+                    case 'btm-right':
+                        watermark.css({'left':maxPosX, 'top':maxPosY});
+                        _setCoordinate(maxPosX,maxPosY);   
+                        break;                       
+                        
+                }
+
         };
 
     return {
@@ -162,34 +217,3 @@ var opacitySlider = (function() {
 }());
 
 opacitySlider.init();
-
-//------------Reset function----------------
-
-var resetForm = (function(){
-    var resetInit = function(){
-        _setupListners();
-    };
-
-    var _setupListners = function(){
-        $('.buttons__reset').on('click', function(){
-            $('.image-view__water-img').css('left', 0),
-            $('.image-view__water-img').css('top', 0),
-                $('.position-right__input-top').spinner('value', 0),
-                $('.position-right__input-bot').spinner('value', 0),
-                $('.opacity__slider').slider("value", 1),
-                $('.image-view__water-img').css('opacity', 1)
-        })
-    };
-
-
-    return{
-        init: resetInit
-    }
-}());
-
-resetForm.init();
-
-
-
-
-
