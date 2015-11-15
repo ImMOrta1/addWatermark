a519aad725024f2058d4793a14ec39b8776da812
Ilya Metsler <imetsler@gmail.com>
2015-11-15 23:29:11 +0600

    minor bug fixes

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'compass/import-once/activate'
 http_path = "/app/"
 css_dir = "/app/css"
 sass_dir = "/app/scss"
-images_dir = "/app/img"
+images_dir = "/app/img/"
 javascripts_dir = "/app/js"
 
 # output_style = :expanded or :nested or :compact or :compressed
